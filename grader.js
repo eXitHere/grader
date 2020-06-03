0c41458653a100d5024a9a2db1ff02c3c08ed026
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-06-03 10:34:27 +0700

    tidy up

--- grader.js
+++ grader.js
@@ -1,33 +1,45 @@
-const {process_} = require('./compile_run'); 
+const { process_ } = require('./compile_run');
 const fetch = require('node-fetch');
 
 var tress = require('tress');
 
 var Q_process = tress(function (body, next) {
-    run(body)
-    .then(() => next());
-},1);
+	run(body).then(() => next());
+}, 1);
 
 process.on('message', async (body) => {
-    Q_process.push(body);
+	Q_process.push(body);
 });
-    
-async function run({submitionId, userId, input, output, scorePerCase, sourceCode}){
-    console.log("new compiler!");
-    const result = await process_(sourceCode,input,output,scorePerCase);
-    //TODO : -> json return {submitionId,userId,result,score,time}
-    const body = {
-        submitionId: submitionId,
-        userId:      userId,
-        result:      result.result,
-        score:       result.score,
-        time:        0
-    }
-    fetch('http://localhost:6666/api/v1/grader_check/', {
-            method:  'post',
-            body:    JSON.stringify(body),
-            headers: { 'Content-Type': 'applicatio/json'},
-        }).then(res => res.JSON())
-        .then(json  => console.log(json));
-    console.log(result);
+
+async function run({
+	questionId,
+	userId,
+	input,
+	output,
+	scorePerCase,
+	sourceCode,
+}) {
+	console.log('new compiler!');
+	const { result, score } = await process_(
+		sourceCode,
+		input,
+		output,
+		scorePerCase
+	);
+	//TODO : -> json return {submitionId,userId,result,score,time}
+	const body = {
+		questionId,
+		userId,
+		result,
+		score,
+		time: 0,
+	};
+	await fetch('http://localhost:5000/api/v1/grader_check/', {
+		method: 'post',
+		body: JSON.stringify(body),
+		headers: { 'Content-Type': 'application/json' },
+	})
+		.then((res) => res.json())
+		.then((json) => console.log(json));
+	console.log(body);
 }
