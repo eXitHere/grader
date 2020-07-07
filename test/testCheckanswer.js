0b69aed5ed11a7cd16432bfe446971c0d5c8e15f
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2020-07-07 12:43:04 +0700

    เปลี่ยนเป็น trim

--- test/testCheckanswer.js
+++ test/testCheckanswer.js
@@ -3,18 +3,19 @@ var test2 = "ABCDEF\nABCDEF\n"
 
 var ans = test.replace(/\s*$/,'');
 
-if(checkAnswer(test,test2)) console.log("Same");
+/*if(checkAnswer(test,test2)) console.log("Same");
 else console.log("Not Same")
+*/
 
 function checkAnswer(master, ans) {
-    var masterSplit = master.replace(/\n+$/, "").split(/\r?\n/);
-    var ansSplit = ans.replace(/\n+$/, "").split(/\r?\n/);
+    var masterSplit = master.trimEnd().split(/\r?\n/);
+    var ansSplit = ans.trimEnd().split(/\r?\n/);
     if (masterSplit.length != ansSplit.length) {
         return false;
     }
     else {
         for(var index = 0; index < masterSplit.length; index++) {
-            if (masterSplit[index].replace(/\s*$/,'') != ansSplit[index].replace(/\s*$/,'')) {
+            if (masterSplit[index].trimEnd() != ansSplit[index].trimEnd()) {
                 return false;
             }
         }
